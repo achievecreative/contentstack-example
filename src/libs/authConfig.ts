b95b481325124267e8cacd1a4ddc2b2c0878f929
Owen <hi@achievecreative.com>
2024-02-26 10:24:45 +0800

    wip

--- src/libs/authConfig.ts
+++ src/libs/authConfig.ts
@@ -1,13 +1,19 @@
 // Config object to be passed to Msal on creation
 export const msalConfig = {
   auth: {
-    clientId: "b5c2e510-4a17-4feb-b219-e55aa5b74144",
-    authority: "https://login.microsoftonline.com/common",
-    redirectUri: "/",
-    postLogoutRedirectUri: "/",
+    clientId: process.env.AZURE_AD_B2C_CLIENT_ID,
+    authority: `https://${process.env.AZURE_AD_B2C_TENANT_ID}.ciamlogin.com/`,
+    clientSecret: process.env.AZURE_AD_B2C_CLIENT_SECRET, // Client secret generated from the app registration in Azure portal
   },
   system: {
-    allowNativeBroker: false, // Disables WAM Broker
+    allowNativeBroker: false, // Disables WAM Broker,
+    loggerOptions: {
+      loggerCallback(loglevel: any, message: any, containsPii: any) {
+        console.log(loglevel, message, containsPii);
+      },
+      piiLoggingEnabled: false,
+      logLevel: "Info",
+    },
   },
 };
 
